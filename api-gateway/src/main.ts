e2cf7e5995be6096521b86bdb8a35da68468f3f2
pedromm65 <pedromm65@hotmail.com.br>
2022-11-13 00:45:02 -0300

    Interceptors

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import momentTimezone from 'moment-timezone';
+import * as momentTimezone from 'moment-timezone';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filters/http-exception.filter';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor())
   app.useGlobalFilters(new AllExceptionsFilter())
   
   Date.prototype.toJSON = function(): any {
